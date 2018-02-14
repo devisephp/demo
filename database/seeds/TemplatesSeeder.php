0dd88c8f74d59be678a4eb4dd476c254593570ee
Mauro Velez <mauro@lbm.co>
2018-02-13 20:45:23 -0500

    removed mothership

--- database/seeds/TemplatesSeeder.php
+++ database/seeds/TemplatesSeeder.php
@@ -13,7 +13,7 @@ class TemplatesSeeder extends Seeder
         'id'         => 1,
         'name'       => 'Home Page',
         'layout'     => 'layouts.marketing',
-        'slots'      => '[{"id":1,"type":"single","slices":[]},{"id":2,"type":"single","slices":[]},{"id":3,"type":"single","slices":[{"id":4,"type":"repeats"}]},{"id":5,"type":"single","slices":[{"id":6,"type":"repeats"}]},{"id":7,"type":"single","slices":[]}]',
+        'slices'      => '[{"id":1,"type":"single","slices":[]},{"id":2,"type":"single","slices":[]},{"id":3,"type":"single","slices":[{"id":4,"type":"repeats"}]},{"id":5,"type":"single","slices":[{"id":6,"type":"repeats"}]},{"id":7,"type":"single","slices":[]}]',
         'created_at' => date('Y-m-d H:i:s'),
         'updated_at' => date('Y-m-d H:i:s'),
       ]
