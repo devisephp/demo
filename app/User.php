cd9a23146aace39e2704220d8ae0ffc8ba4f9c1d
Mauro Velez <mauro@lbm.co>
2018-02-15 09:46:20 -0500

    trying to remove groups

--- app/User.php
+++ app/User.php
@@ -26,9 +26,4 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    public function groups()
-    {
-      return $this->belongsToMany(Group::class);
-    }
 }
