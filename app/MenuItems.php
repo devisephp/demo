033fc74bc7a32ef1df490d8600fcfe223e087528
Mauro Velez <mauro@lbm.co>
2018-03-14 11:09:06 -0400

    added column config for super table

--- app/MenuItems.php
+++ app/MenuItems.php
@@ -9,9 +9,31 @@ use Illuminate\Database\Eloquent\Model;
 class MenuItems extends Model
 {
   use Filterable, Sortable;
-  
+
   public $slice = [
     'name',
     'url'
+  ];
+
+  public $tableColumns = [
+    [
+      "key"    => 'id',
+      "label"  => 'ID',
+      "sort"   => 'id',
+      "search" => 'id'
+    ],
+    [
+      "key"    => 'name',
+      "label"  => 'Name',
+      "sort"   => 'name',
+      "search" => 'name'
+    ],
+    [
+      "key"    => 'url',
+      "label"  => 'URL',
+      "sort"   => 'url',
+      "search" => 'url',
+      "show"   => false
+    ]
   ];
 }
