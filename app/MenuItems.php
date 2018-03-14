67f86ae5f1545a557acad528f22b3d96de672fbf
Mauro Velez <mauro@lbm.co>
2018-03-14 10:46:30 -0400

    added traits to menu items and changed up templates seeder to match db

--- app/MenuItems.php
+++ app/MenuItems.php
@@ -2,10 +2,14 @@
 
 namespace App;
 
+use Devise\Traits\Filterable;
+use Devise\Traits\Sortable;
 use Illuminate\Database\Eloquent\Model;
 
 class MenuItems extends Model
 {
+  use Filterable, Sortable;
+  
   public $slice = [
     'name',
     'url'
