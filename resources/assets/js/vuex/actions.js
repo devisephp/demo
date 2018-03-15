fbd47e48e3fb2bd052d4d7ef0b3965b90261e901
Gary Williams <gary@lbm.co>
2018-03-14 20:28:27 -0400

    Actions url names-acing

--- resources/assets/js/vuex/actions.js
+++ resources/assets/js/vuex/actions.js
@@ -2,8 +2,9 @@ const actions = {
 
   // Menus
   getMenus (context) {
+    console.log(context)
     return new Promise((resolve, reject) => {
-      window.axios.get(context.state.api.baseUrl + 'menus/').then(function (response) {
+      window.axios.get(context.state.devise.api.baseUrl + 'menu-items/').then(function (response) {
         context.commit('setMenus', response.data)
         resolve(response)
       }).catch(function (error) {
@@ -16,7 +17,7 @@ const actions = {
 
   createMenu (context, menu) {
     return new Promise((resolve, reject) => {
-      window.axios.post(context.state.api.baseUrl + 'menus/', menu).then(function (response) {
+      window.axios.post(context.state.devise.api.baseUrl + 'menu-items/', menu).then(function (response) {
         window.bus.$emit('showMessage', {title: 'Success!', message: menu.title + ' has been created.'})
         context.commit('createMenu', response.data.data)
         resolve(response)
@@ -30,7 +31,7 @@ const actions = {
 
   updateMenu (context, payload) {
     return new Promise((resolve, reject) => {
-      window.axios.put(context.state.api.baseUrl + 'menus/' + payload.menu.id, payload.data).then(function (response) {
+      window.axios.put(context.state.devise.api.baseUrl + 'menu-items/' + payload.menu.id, payload.data).then(function (response) {
         window.bus.$emit('showMessage', {title: 'Success!', message: payload.data.title + ' has been saved.'})
         context.commit('updateMenu', {menu: menu, data: response.data})
         resolve(response)
@@ -44,7 +45,7 @@ const actions = {
 
   deleteMenu (context, menu) {
     return new Promise((resolve, reject) => {
-      window.axios.delete(context.state.api.baseUrl + 'menus/' + menu.id).then(function (response) {
+      window.axios.delete(context.state.devise.api.baseUrl + 'menu-items/' + menu.id).then(function (response) {
         window.bus.$emit('showMessage', {title: 'Success!', message: menu.title + ' has been deleted.'})
         context.commit('deleteMenu', menu)
         resolve(response)
