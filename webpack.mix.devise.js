6e2fc71c2b8233196f4631f6519d25a59c75fded
Gary Williams <gary@lbm.co>
2018-03-21 11:28:18 -0400

    Getting ready to setup new environment

--- webpack.mix.devise.js
+++ webpack.mix.devise.js
@@ -2,8 +2,8 @@ let { mix } = require('laravel-mix');
 let tailwindcss = require('tailwindcss');
 
 mix
-  .setPublicPath(path.normalize('public/assets/devise'))
-  .js('resources/assets/js/app.js', 'public/js')
+  .setPublicPath(path.normalize('public/devise-assets/devise'))
+  .js('resources/assets/js/app.js', 'js')
   .options({
     processCssUrls: false,
     postCss: [ tailwindcss('./resources/assets/js/devise-dev/tailwind/tailwind.js') ],
--- webpack.mix.website.js
+++ webpack.mix.website.js
@@ -2,8 +2,8 @@ let { mix } = require('laravel-mix');
 let tailwindcss = require('tailwindcss');
 
 mix
-  .setPublicPath(path.normalize('public/assets/website'))
-  .sass('resources/assets/sass/app.scss', 'public/css')
+  .setPublicPath(path.normalize('public'))
+  .sass('resources/assets/sass/app.scss', 'css')
   .options({
     processCssUrls: false,
     postCss: [ tailwindcss('./tailwind.js') ],
