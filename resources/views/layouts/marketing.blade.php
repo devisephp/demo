8fc3cd05fd8e34c43237ab55fd9e56e2d8b1f8eb
Gary Williams <gary@lbm.co>
2018-03-21 14:57:57 -0400

    Getting ready for real installer

--- resources/views/layouts/marketing.blade.php
+++ resources/views/layouts/marketing.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <link rel="stylesheet" href="{{ mix('/public/css/app.css', '/assets/website/') }}" />
+    <link rel="stylesheet" href="{{ mix('/css/app.css', 'devise') }}" />
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}" />
 
     <title>Welcome to Devise Cruises</title>
 
@@ -26,7 +27,9 @@
       </devise>
     </div>
 
-    <script src="{{mix('/public/js/app.js', '/assets/devise')}}"></script>
+    <script src="{{mix('/js/manifest.js', '/devise')}}"></script>
+    <script src="{{mix('/js/vendor.js', '/devise')}}"></script>
+    <script src="{{mix('/js/app.js', '/devise')}}"></script>
 
   </body>
 </html>
--- webpack.mix.devise.js
+++ webpack.mix.devise.js
@@ -2,8 +2,9 @@ let { mix } = require('laravel-mix');
 let tailwindcss = require('tailwindcss');
 
 mix
-  .setPublicPath(path.normalize('public/devise-assets/devise'))
+  .setPublicPath(path.normalize('public/devise'))
   .js('resources/assets/js/app.js', 'js')
+  .sass('resources/assets/js/devise-dev/src/sass/devise.scss', 'css')
   .options({
     processCssUrls: false,
     postCss: [ tailwindcss('./resources/assets/js/devise-dev/tailwind/tailwind.js') ],
