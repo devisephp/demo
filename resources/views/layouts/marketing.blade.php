e3b41f5c1fffbde43eeda0000c75b67a29406039
Gary Williams <gary@lbm.co>
2018-03-22 12:26:56 -0400

    tweaking for building

--- resources/views/layouts/marketing.blade.php
+++ resources/views/layouts/marketing.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    {{-- <link rel="stylesheet" href="{{ mix('/css/app.css', 'devise') }}" /> --}}
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}" />
 
     <title>Welcome to Devise Cruises</title>
--- webpack.mix.devise.js
+++ webpack.mix.devise.js
@@ -1,13 +1,15 @@
 let { mix } = require('laravel-mix');
 let tailwindcss = require('tailwindcss');
+require('laravel-mix-purgecss');
 
 mix
-  // .setPublicPath(path.normalize('public/devise'))
+  .setPublicPath(path.normalize('public/devise'))
   .js('resources/assets/js/app.js', 'js')
   // .sass('resources/assets/js/devise-dev/src/sass/devise.scss', 'css')
   .options({
     processCssUrls: false,
     postCss: [ tailwindcss('./resources/assets/js/devise-dev/src/tailwind/tailwind.js') ],
   })
+  .copy('node_modules/font-awesome/fonts', 'public/fonts')
+  .purgeCss()
   .version()
-  .copy('node_modules/font-awesome/fonts', 'public/fonts');
